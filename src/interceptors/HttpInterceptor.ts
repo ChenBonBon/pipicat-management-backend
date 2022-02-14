2f53cc8814e8aded33efed89b154ac48389c48dd
BonBon <bonbon.chen@basebit.ai>
2022-02-14 15:43:45 +0800

    更新日志格式

--- src/interceptors/HttpInterceptor.ts
+++ src/interceptors/HttpInterceptor.ts
@@ -7,15 +7,18 @@ import {
 } from '@nestjs/common';
 import { Observable } from 'rxjs';
 
-const parseQuery = (query) => {
-  const queries = query.split('&');
+const parseQuery = (query: string) => {
   const res = {};
-  if (queries.length > 0) {
-    queries.forEach((query) => {
-      const key = query.split('=')[0];
-      const value = query.split('=')[1];
-      res[key] = { value, type: typeof value };
-    });
+
+  if (query) {
+    const queries = query.split('&');
+    if (queries.length > 0) {
+      queries.forEach((query) => {
+        const key = query.split('=')[0];
+        const value = query.split('=')[1];
+        res[key] = { value, type: typeof value };
+      });
+    }
   }
 
   return res;
@@ -25,10 +28,17 @@ const parseQuery = (query) => {
 export class HttpInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     const request = context.switchToHttp().getRequest();
-    const { pathname, query } = request._parsedUrl;
+    const { method, body, _parsedUrl } = request;
+    const { pathname, query } = _parsedUrl;
+
     const logger = new Logger(context.getClass().name);
+
     logger.log(
-      JSON.stringify({ pathname, query: parseQuery(query) }, null, '\t'),
+      JSON.stringify(
+        { path: `${method} ${pathname}`, query: parseQuery(query), body },
+        null,
+        '\t',
+      ),
     );
     return next.handle();
   }
